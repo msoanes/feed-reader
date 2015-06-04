f84420e44f9190748df6655dd964fb7d67e43812
App Academy Student <student@appacademy.io>
2015-06-04 11:42:01 -0700

    Add getOrFetch to Feeds collection

--- app/assets/javascripts/collections/feeds.js
+++ app/assets/javascripts/collections/feeds.js
@@ -1,5 +1,19 @@
 NewsReader.Collections.Feeds = Backbone.Collection.extend({
   url: '/api/feeds',
 
-  model: NewsReader.Models.Feed
+  model: NewsReader.Models.Feed,
+
+  getOrFetch: function (id, callback) {
+    var collection = this;
+    var model = this.get(id);
+    if (typeof model === 'undefined') {
+      model = new NewsReader.Models.Feed({id: id});
+      model.fetch({
+        success: function () {
+          collection.add(model);
+        }
+      });
+    }
+    return model;
+  }
 });
