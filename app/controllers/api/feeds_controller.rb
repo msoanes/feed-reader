0d132b4b5939c7fe224cba18f3875760448c3e03
App Academy Student <student@appacademy.io>
2015-06-04 14:27:51 -0700

    Add destroy action to Feed controller

--- app/controllers/api/feeds_controller.rb
+++ app/controllers/api/feeds_controller.rb
@@ -10,10 +10,16 @@ class Api::FeedsController < ApplicationController
   def create
     feed = Feed.find_or_create_by_url(feed_params[:url])
     if feed
-      render :json => feed
+      render json: feed
     else
-      render :json => { error: "invalid url" }, status: :unprocessable_entity
+      render json: { error: "invalid url" }, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    feed = Feed.find(params[:id])
+    feed.destroy
+    render json: feed
   end
 
   private
