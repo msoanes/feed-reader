7c5fdf256bd07a56e01f455ef2a3f8408ea33026
App Academy Student <student@appacademy.io>
2015-06-04 14:28:31 -0700

    Render FeedsIndex items as subviews, and add a delete button

--- app/assets/javascripts/views/feeds/index.js
+++ app/assets/javascripts/views/feeds/index.js
@@ -1,13 +1,31 @@
-NewsReader.Views.FeedsIndex = Backbone.View.extend({
+NewsReader.Views.FeedsIndex = Backbone.CompositeView.extend({
   template: JST['feeds/index'],
 
   initialize: function () {
+    this.collection.each(function(feed) {
+      var subView = new NewsReader.Views.FeedIndexItem({model: feed});
+      this.addSubview('ul.feeds', subView);
+    }.bind(this));
+
+    this.listenTo(this.collection, 'add', this.updateSubview);
+    this.listenTo(this.collection, 'remove', this.removeFeed);
     this.listenTo(this.collection, 'sync', this.render);
   },
 
   render: function () {
     var renderedContent = this.template({ feeds: this.collection });
     this.$el.html(renderedContent);
+    this.attachSubviews();
     return this;
+  },
+
+  updateSubview: function(model) {
+    var subView = new NewsReader.Views.FeedIndexItem({model: model});
+    this.addSubview('ul.feeds', subView);
+  },
+
+  removeFeed: function(model) {
+    console.log('Removing feed');
+    this.removeModelSubview('ul.feeds', model);
   }
 });
---
+++ app/assets/javascripts/views/feeds/index_item.js
@@ -0,0 +1,26 @@
+NewsReader.Views.FeedIndexItem = Backbone.View.extend({
+  template: JST['feeds/index_item'],
+
+  events: {
+    'click button.delete': 'deleteFeed'
+  },
+
+  initialize: function () {
+    this.listenTo(this.model, 'sync', this.render);
+  },
+
+  tagName: 'li',
+
+  render: function () {
+    console.log('rendering');
+    var renderedContent = this.template({feed: this.model});
+    this.$el.html(renderedContent);
+    return this;
+  },
+
+  deleteFeed: function (event) {
+    console.log("In deleteFeed");
+    // event.preventDefault();
+    this.model.destroy();
+  }
+});
